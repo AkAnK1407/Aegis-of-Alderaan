30a2ee4dc6bdfeb7a55d609873f50c4bebaf09ed
Hackeries <itssaj15@gmail.com>
2025-12-02 20:41:57 +0530

    updated components

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+const container = document.getElementById("root");
+
+if (!container) {
+  throw new Error(
+    'Root container (#root) not found. Ensure index.html contains <div id="root"></div>.'
+  );
+}
+
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
